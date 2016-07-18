d792e3927435f4e6e6860b2f7e02d5bf5540e17f
agulado <9948686@qq.com>
2016-07-18 19:26:40 +0800

    1. 解决了一下之前的bug：页面配合mobile_stop_moved时不正常，和弹出软键盘时不正常
    解决思路：
    
    经监听发现：在resize时，共调用了3次risize监听事件，第一次是旋转前，另两次是旋转后。
    
    so，为解决安卓软键盘resize的bug：
    	打开页面时，记录朝向。发生risize时，判断记录的朝向是否等于新的朝向。
    是的话，隐藏遮罩并终止运行；否则显示遮罩（遮罩只有在横屏时才被显示，css的media）。
    
    为解决resize3次触发事件的问题，重置屏幕朝向后，不更新记录的朝向。也就是记录的朝向永远为0。
    也能理解为，发生resize时，判断当前朝向：如果是0，则不显示遮罩；如果不是0，则根据是否横屏显示遮罩。
    
    为解决横屏后高度不正确问题：resize事件通过朝向判断后，重置遮罩层高度前，让所有input,select失去焦点。
    
    2. 之前版本 可以阻止遮罩的class="landscape_mask_no_focus" 不再使用。现在只要横屏就会遮罩。

--- README.md
+++ README.md
@@ -1,22 +1,41 @@
-# Landscape_mask v1.1.1
+# Landscape_mask v1.1.2
 ### 横屏遮罩（主要移动端使用）
 ### 兼容原生JS规范和AMD规范
 
 更新日志
 --------------
+v1.1.2
+
+		1. 解决了一下之前的bug：页面配合mobile_stop_moved时不正常，和弹出软键盘时不正常
+
+		解决思路：
+
+		经监听发现：在resize时，共调用了3次risize监听事件，第一次是旋转前，另两次是旋转后。
+		
+		so，为解决安卓软键盘resize的bug：
+			打开页面时，记录朝向。发生risize时，判断记录的朝向是否等于新的朝向。
+		是的话，隐藏遮罩并终止运行；否则显示遮罩（遮罩只有在横屏时才被显示，css的media）。
+
+		为解决resize3次触发事件的问题，重置屏幕朝向后，不更新记录的朝向。也就是记录的朝向永远为0。
+		也能理解为，发生resize时，判断当前朝向：如果是0，则不显示遮罩；如果不是0，则根据是否横屏显示遮罩。
+
+		为解决横屏后高度不正确问题：resize事件通过朝向判断后，重置遮罩层高度前，让所有input,select失去焦点。
+
+		2. 之前版本 可以阻止遮罩的class="landscape_mask_no_focus" 不再使用。现在只要横屏就会遮罩。
+
 v1.1.1
 
-1. 兼容原生JS和AMD规范
-2. 修改demo
+		1. 兼容原生JS和AMD规范
+		2. 修改demo
 
 v1.0.2
 
-1. 修改弹出层的定位为fixed
+		1. 修改弹出层的定位为fixed
 
 文件结构：
 -------------
-1. landscape_mask.js放入项目文件夹jq（原生规范）或widget/lib（AMD规范）中
-2. landscape_mask.min.css、landscape_mask.min.css.map、landscape_mask.png 放入项目文件夹inc中
+		1. landscape_mask.js放入项目文件夹jq（原生规范）或widget/lib（AMD规范）中
+		2. landscape_mask.min.css、landscape_mask.min.css.map、landscape_mask.png 放入项目文件夹inc中
 
 页面引用：
 -------------
--- demo/jq/landscape_mask.js
+++ demo/jq/landscape_mask.js
@@ -1,5 +1,5 @@
 /*
- *@ 1.1.1
+ *@ 1.1.2
  *@ 高京
  *@ 20151009
  *@ 判断是否竖屏，横屏自动出黑屏动画
@@ -8,32 +8,46 @@ var landscape_mask = {
 
     init: function() {
 
+        // 加载样式
         landscape_mask.includeCSS("/inc/landscape_mask.min.css");
 
+        // 动态增加遮罩DOM
         var tag_str = "<section class=\"landscape_mask\">" + "<div class=\"mod_div\">" + "<img src=\"/inc/landscape_mask.png\" />" + "<p>为了更好的体验，请使用竖屏浏览</p>" + "</div>" + "</section>";
-
         $("body").append(tag_str);
 
         var _selector = $("section.landscape_mask");
 
-        var changeDo = function() {
+        $("p.a").html("0");
 
-            if ($(".landscape_mask_no_focus:focus").length > 0) {
-                if (_selector.hasClass("landscape_mask_show"))
-                    _selector.removeClass("landscape_mask_show");
-                return;
-            } else if (!_selector.hasClass("landscape_mask_show"))
-                _selector.addClass('landscape_mask_show');
+        // 发生屏幕旋转和打开页面时，重置遮罩层高度
+        var changeDo = function() {
 
             var window_height_px = $(window).height();
 
             _selector.css("height", window_height_px + "px");
+
+            $("p.a").html($("p.a").html() + "<br />32:" + window.orientation);
         };
 
-        changeDo();
-
+        // 监听窗口大小改变（安卓弹出软键盘时，也会触发）
+        var resize_n = 0;
         $(window).resize(function() {
-            changeDo();
+            // 如果当前屏幕朝向等于0，则隐藏遮罩，并return（屏蔽安卓input获得焦点后的resize）。
+            // 如朝向不为0，则显示遮罩，并先使所有input,select失去焦点（收起软键盘）后，再重置遮罩层高度
+            $("p.a").html($("p.a").html() + "<br />39:window-" + window.orientation);
+            if (window.orientation == 0) {
+                _selector.removeClass("landscape_mask_show");
+                return;
+            } else if(!_selector.hasClass("landscape_mask_show"))
+                _selector.addClass("landscape_mask_show");
+
+            $("input,select").blur();
+            if (resize_n++ > 0)
+                return;
+            setTimeout(function() {
+                changeDo();
+                resize_n = 0;
+            }, 0);
         });
     },
 
@@ -47,10 +61,9 @@ var landscape_mask = {
     }
 };
 
-if(typeof define==="function" && define.amd){
-    define(function(){
+if (typeof define === "function" && define.amd) {
+    define(function() {
         return landscape_mask;
     })
-}
-else
+} else
     landscape_mask.init();
